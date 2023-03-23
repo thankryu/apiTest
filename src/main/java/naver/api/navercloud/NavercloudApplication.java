aff854504987b63962d04713f4c4303910e47223
Kyounghan Ryu <thankryu@gmail.com>
2023-03-23 18:20:15 +0900

    First Setting

---
+++ src/main/java/naver/api/navercloud/NavercloudApplication.java
@@ -0,0 +1,13 @@
+package naver.api.navercloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavercloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NavercloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/naver/api/navercloud/NavercloudApplicationTests.java
@@ -0,0 +1,13 @@
+package naver.api.navercloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavercloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
